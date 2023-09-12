8c937f73ff090124c4018b045c4b9d5ba0bf3fd2
imgonnabe <qorlwn456123@gmail.com>
2023-09-12 14:49:40 +0900

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloVue ></HelloVue>
+  <CountOne></CountOne>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountOne from './components/CountOne.vue'
+import HelloVue from './components/HelloVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CountOne,
+    HelloVue
   }
 }
 </script>
---
+++ src/components/HelloVue.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    {{name}}
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:"HelloVue",
+    data(){
+        return {
+            name:"홍길동"
+        }
+    }
+}
+</script>
+
+<style>
+    div{
+        height: 100px;
+        width: 500px;
+        background-color: gray;
+    }
+    .hello{
+        background-color: green;
+    }
+</style>
